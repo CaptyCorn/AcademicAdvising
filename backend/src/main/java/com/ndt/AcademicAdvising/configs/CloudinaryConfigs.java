416b113c1914e985d1bb0e2786ae92bb0e7a317d
ngodo0704@gmail.com <ngodo0704@gmail.com>
2026-08-04 10:14:44 +0700

    Update: security congfig

---
+++ backend/src/main/java/com/ndt/AcademicAdvising/configs/CloudinaryConfigs.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ndt.AcademicAdvising.configs;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+/**
+ *
+ * @author ngodo
+ */
+@Configuration
+@PropertySource("classpath:application.properties")
+public class CloudinaryConfigs {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        Cloudinary cloudinary
+                = new Cloudinary(ObjectUtils.asMap(
+                        "cloud_name", this.env.getProperty("cloudinary.cloud_name"),
+                        "api_key", this.env.getProperty("cloudinary.api_key"),
+                        "api_secret", this.env.getProperty("cloudinary.api_secret"),
+                        "secure", true));
+        return cloudinary;
+    }
+}
