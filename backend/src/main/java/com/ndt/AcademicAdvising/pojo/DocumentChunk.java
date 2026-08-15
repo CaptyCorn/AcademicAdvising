4d7664a0f4c439b31ccf378555c275ab151eca99
ngodo0704@gmail.com <ngodo0704@gmail.com>
2026-08-15 15:17:39 +0700

    chore: edit DocumentChunk entity, delete Document entity and add some dependencies

--- backend/src/main/java/com/ndt/AcademicAdvising/pojo/DocumentChunk.java
+++ backend/src/main/java/com/ndt/AcademicAdvising/pojo/DocumentChunk.java
@@ -7,22 +7,11 @@ package com.ndt.AcademicAdvising.pojo;
 import com.pgvector.PGvector;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.util.Date;
 import java.util.Map;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
+import java.util.UUID;
 import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.type.SqlTypes;
 
 /**
@@ -33,65 +22,52 @@ import org.hibernate.type.SqlTypes;
 @Table(name = "tbl_document_chunk")
 public class DocumentChunk {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Integer id;
-    @Column
-    @Lob
-    private String textChunk;
+    @Column(name = "embedding_id")
+    private UUID embeddingId;
+    @Column(columnDefinition = "TEXT")
+    private String text;
     @JdbcTypeCode(SqlTypes.JSON)
     @Column(columnDefinition = "jsonb")
     private Map<String, Object> metadata;
     @Column(columnDefinition = "vector(1536)")
     private PGvector embedding;
-    @CreationTimestamp
-    @Column(name = "created_at", updatable = false)
-    private Date createdAt;
-    @UpdateTimestamp
-    @Column(name = "updated_at")
-    private Date updatedAt;
-    @JoinColumn(name = "document_id", referencedColumnName = "id")
-    @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    private Document document;
 
     public DocumentChunk() {
     }  
 
-    public DocumentChunk(Integer id, String textChunk, Map<String, Object> metadata, PGvector embedding, Date createdAt, Date updatedAt, Document document) {
-        this.id = id;
-        this.textChunk = textChunk;
+    public DocumentChunk(UUID embeddingId, String text, Map<String, Object> metadata, PGvector embedding) {
+        this.embeddingId = embeddingId;
+        this.text = text;
         this.metadata = metadata;
         this.embedding = embedding;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.document = document;
     }
 
     /**
-     * @return the id
+     * @return the embeddingId
      */
-    public Integer getId() {
-        return id;
+    public UUID getEmbeddingId() {
+        return embeddingId;
     }
 
     /**
-     * @param id the id to set
+     * @param embeddingId the embeddingId to set
      */
-    public void setId(Integer id) {
-        this.id = id;
+    public void setEmbeddingId(UUID embeddingId) {
+        this.embeddingId = embeddingId;
     }
 
     /**
-     * @return the textChunk
+     * @return the text
      */
-    public String getTextChunk() {
-        return textChunk;
+    public String getText() {
+        return text;
     }
 
     /**
-     * @param textChunk the textChunk to set
+     * @param text the text to set
      */
-    public void setTextChunk(String textChunk) {
-        this.textChunk = textChunk;
+    public void setText(String text) {
+        this.text = text;
     }
 
     /**
@@ -122,47 +98,6 @@ public class DocumentChunk {
         this.embedding = embedding;
     }
 
-    /**
-     * @return the createdAt
-     */
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    /**
-     * @param createdAt the createdAt to set
-     */
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    /**
-     * @return the updatedAt
-     */
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    /**
-     * @param updatedAt the updatedAt to set
-     */
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    /**
-     * @return the document
-     */
-    public Document getDocument() {
-        return document;
-    }
-
-    /**
-     * @param document the document to set
-     */
-    public void setDocument(Document document) {
-        this.document = document;
-    }
-
+    
     
 }
